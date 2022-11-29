1512e19edadb7362a17312ca07f2db8f0161b814
Muhammad Bilal <bilalakbar@technerds.com>
2022-11-29 20:54:01 +0500

    Select Option Functionality

--- app/views.py
+++ app/views.py
@@ -1,22 +1,18 @@
 import os
 from flask import render_template, request, url_for, redirect, send_from_directory
 from flask_login import login_user, logout_user, current_user
-from jinja2 import TemplateNotFound
 import pandas as pd
 from .ml_recidivism import GetDataVisualizations
-# App modules
-from app import app, lm, db, bc
+from app import app, lm, bc
 from app.models import Users
 from app.forms import LoginForm, RegisterForm
 
 
-# provide login manager with load_user callback
 @lm.user_loader
 def load_user(user_id):
     return Users.query.get(int(user_id))
 
 
-# Logout user
 @app.route('/logout/')
 def logout():
     logout_user()
@@ -25,19 +21,29 @@ def logout():
 
 @app.route('/import-data/', methods=['GET', 'POST'])
 def import_data():
-
     msg = None
     success = False
 
     if request.method == 'GET':
-        return render_template('import_data.html', msg=msg )
+        return render_template('import_data.html', msg=msg)
 
     if request.method == 'POST':
         excel_sheet = request.files["excelsheet"]
         if excel_sheet:
             data = pd.read_csv(excel_sheet, error_bad_lines=False)
             visualizer = GetDataVisualizations()
-            visualizer.live_visualization(data)
+
+            if request.form.get("convicting_offence", False):
+                visualizer.convicting_offense(data)
+                return render_template('import_data.html', msg=msg)
+            if request.form.get("age_release", False):
+                visualizer.age_at_release(data)
+                return render_template('import_data.html', msg=msg)
+            if request.form.get("not_released", False):
+                visualizer.who_didnt_return(data)
+                return render_template('import_data.html', msg=msg)
+
+            msg = "Select at least one option!"
             return render_template('import_data.html', msg=msg)
         else:
             msg = "Excel sheet Required"
@@ -51,7 +57,6 @@ def import_data():
 # Register a new user
 @app.route('/register/', methods=['GET', 'POST'])
 def register():
-
     # declare the Registration Form
     form = RegisterForm(request.form)
 
@@ -59,8 +64,7 @@ def register():
     success = False
 
     if request.method == 'GET':
-
-        return render_template( 'register.html', form=form, msg=msg )
+        return render_template('register.html', form=form, msg=msg)
 
     # check if both http method is POST and form is valid on submit
     if form.validate_on_submit():
@@ -93,13 +97,11 @@ def register():
     else:
         msg = 'Input error'
 
-    return render_template( 'register.html', form=form, msg=msg, success=success )
+    return render_template('register.html', form=form, msg=msg, success=success)
 
 
-# Authenticate user
 @app.route('/login/', methods=['GET', 'POST'])
 def login():
-    
     # Declare the login form
     form = LoginForm(request.form)
 
@@ -111,13 +113,13 @@ def login():
 
         # assign form data to variables
         username = request.form.get('username', '', type=str)
-        password = request.form.get('password', '', type=str) 
+        password = request.form.get('password', '', type=str)
 
         # filter User out of database through username
         user = Users.query.filter_by(user=username).first()
 
         if user:
-            
+
             if bc.check_password_hash(user.password, password):
                 login_user(user)
                 return redirect(url_for('index'))
@@ -126,29 +128,51 @@ def login():
         else:
             msg = "Unknown user"
 
-    return render_template( 'login.html', form=form, msg=msg )
+    return render_template('login.html', form=form, msg=msg)
 
 
-# App main route + generic routing
 @app.route('/', defaults={'path': 'index'})
 @app.route('/<path>')
 def index(path):
     if not current_user.is_authenticated:
         return redirect(url_for('login'))
-
     else:
-        try:
-
+        if request.method == "GET":
             return render_template('index.html')
 
-        except TemplateNotFound:
-            return render_template('page-404.html'), 404
 
-        except:
-            return render_template('page-500.html'), 500
+@app.route('/import-web-view/', methods=['POST', 'GET'])
+def web_view():
+        if request.method == "POST":
+            msg = None
+            success = True
+            excel_sheet = request.files["excelsheet"]
+            if excel_sheet:
+                data = pd.read_csv(excel_sheet, error_bad_lines=False)
+                visualizer = GetDataVisualizations()
+
+                if request.form.get("convicting_offence", False):
+                    visualizer.convicting_offense(data)
+                if request.form.get("age_release", False):
+                    visualizer.age_at_release(data)
+
+                if request.form.get("not_released", False):
+                    visualizer.who_didnt_return(data)
+                else:
+                    msg = "Select at least one option!"
+                    return render_template('index.html', msg=msg)
+
+                return render_template('index.html', msg=msg)
+            else:
+                msg = "Excel sheet Required"
+            return render_template('index.html', msg=msg, success=success)
+        else:
+            if not current_user.is_authenticated:
+                return render_template('index.html')
+            else:
+                    return render_template('index.html')
 
 
-# Return sitemap
 @app.route('/sitemap.xml/')
 def sitemap():
     return send_from_directory(os.path.join(app.root_path, 'static'), 'sitemap.xml')
