1b0078e168dfe3f583d41ea02b834f593c9f239c
Muhammad Bilal <bilalakbar@technerds.com>
2022-11-30 17:51:55 +0500

    updates

--- app/ml_recidivism.py
+++ app/ml_recidivism.py
@@ -6,7 +6,7 @@ class GetDataVisualizations:
 
     def convicting_offense(self, data):
         # Plotting race and Convicting Offense Classification to understand the data more
-        plt.figure(figsize=(10, 7))
+        plt.figure(figsize=(10, 10))
         plt.subplot(1, 2, 1)  # row 1, col 2 index 1
         data['Race - Ethnicity'].value_counts().plot(kind='bar')
         plt.title("Race - Ethnicity")
@@ -15,11 +15,13 @@ class GetDataVisualizations:
         plt.subplot(1, 2, 2)  # index 2
         data['Convicting Offense Classification'].value_counts().plot(kind='bar')
         plt.title("Convicting Offense Classification")
-        plt.show()
+        # plt.show()
+        image = plt.savefig('my_plot1.png')
+        print(image)
 
     def age_at_release(self, data):
         # Plotting age and Recidivism
-        plt.figure(figsize=(10, 7))
+        plt.figure(figsize=(10, 10))
         plt.subplot(1, 2, 1)  # row 1, col 2 index 1
         data['Recidivism - Return to Prison numeric'].value_counts().plot(kind='bar')
         plt.title("Recidivism - Return to Prison numeric")
@@ -28,21 +30,25 @@ class GetDataVisualizations:
         plt.subplot(1, 2, 2)  # index 2
         data['Age At Release '].value_counts().plot(kind='bar')
         plt.title("Age At Release ")
-        plt.show()
+        # plt.show()
+
+        image = plt.savefig('my_plot2.png')
+        print(image)
 
     def who_didnt_return(self, data):
         # Seeing if there is a correlation between age and recidivism
-        plt.figure(figsize=(10, 7))
-        plt.subplot(1, 2, 1)  # row 1, col 2 index 1
-        data.loc[data['Recidivism - Return to Prison numeric'] == 1]['Age At Release '].value_counts().plot(kind='bar')
-        plt.title("Age at release of people who returned to prison")
-        plt.ylabel('Count')
-
-        plt.subplot(1, 2, 2)  # index 2
-        data.loc[data['Recidivism - Return to Prison numeric'] == 0]['Age At Release '].value_counts().plot(kind='bar')
-        plt.title("Age at release of people who didn't return")
-        plt.show()
-
+        # plt.figure(figsize=(10, 10))
+        # plt.subplot(1, 2, 1)  # row 1, col 2 index 1
+        # data.loc[data['Recidivism - Return to Prison numeric'] == 1]['Age At Release '].value_counts().plot(kind='bar')
+        # plt.title("Age at release of people who returned to prison")
+        # plt.ylabel('Count')
+        #
+        # plt.subplot(1, 2, 2)  # index 2
+        # data.loc[data['Recidivism - Return to Prison numeric'] == 0]['Age At Release '].value_counts().plot(kind='bar')
+        # plt.title("Age at release of people who didn't return")
+        # # plt.show()
+        # image = plt.savefig('my_plot3.png')
+        # print(image)
         # Cleaning up the data. Here we create new columns for categorical columns like Race, Age, Conviction Offense,
         # and Release type.
         # Machine learning algorithms prefer numbers as input.
@@ -63,9 +69,20 @@ class GetDataVisualizations:
                                             "Convicting Offense Type", "Convicting Offense Subtype", "Release Type",
                                             "Release type: Paroled to Detainder united", "Main Supervising District",
                                             "N/A -", "Part of Target Population"], axis=1)
-        print(final_data)
+
+        # plt.plot(final_data)
+        # plt.show()
+        plt.plot(final_data)
+        image = plt.savefig('my_plot4.png')
+        print(image)
         # Spliting the data into input data (X), and output data (y).
         # The input variables will be the variables that, will be fed into the model and the output
         # variable will be the "Recidivism - Return to Prison numeric".
         X = data.drop(["Recidivism - Return to Prison numeric"], axis=1)
         y = data["Recidivism - Return to Prison numeric"]
+        print("XXXXXXXXXX")
+        print(X)
+        print(y)
+        print("YYYYYYYYYYYYYY")
+        # plt.plot(X, y)
+        # plt.show()
